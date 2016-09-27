114f67c53820f9ab15ec2781722f6a5fdf5995d0
Bftech <corentin.monmasson@gmail.com>
2016-09-27 21:47:33 +0200

    gallery fixed

--- js/master.js
+++ js/master.js
@@ -1,6 +1,21 @@
+var lastA = "";
 
 $(".viewer_element a").click(function () {
-  $(this).toggleClass("hovered");
-  $(".viewer_element").toggleClass("folded");
-  $(this).parent().toggleClass("unfolded");
-})
+  if ($(this).parent().attr("id") == lastA) {
+    $(this).toggleClass("hovered");
+    $(".viewer_element").not($(this).parent()).toggleClass("folded");
+    $(this).parent().toggleClass("unfolded");
+  } else {
+    $(".viewer_element").removeClass("unfolded");
+    $(".viewer_element").addClass("folded");
+    $(".viewer_element a").removeClass("hovered");
+    $(this).parent().removeClass("folded");
+    $(this).parent().addClass("unfolded");
+    $(this).addClass("hovered");
+  }
+  lastA = $(this).parent().attr("id");
+});
+
+$("#navbar a").click(function() {
+  $("#navbar").toggleClass("show");
+});
